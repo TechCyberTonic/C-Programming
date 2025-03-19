506a61b0c2b07b6c43a033a3c33e9dd6cc321f12
TechCyberTonic <hacking.techtonic@gmail.com>
2025-03-19 16:24:54 +0545

    setup in computer

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <stdbool.h>
 int main()
 {
+  bool iscomputer = "True";
   printf("hello world!");
+  if(iscomputer == true)
+  {
+    printf("Setup completed in Computer");
+  }
+  else
+  {
+    printf("Setup recommended!");
+  }
   return 0;
 }
